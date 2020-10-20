1fc441649390729505a837b0f7e3c0f915222ba4
AdiG <adigollamudi@gmail.com>
2020-10-19 23:32:34 -0400

    started basic exp server

--- src/App.js
+++ src/App.js
@@ -1,57 +1,46 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './styles.css';
-import PlayerPage from './PlayerPage'
-import PlayerID from './PlayerID'
-
+import PlayerPage from './PlayerPage';
+import PlayerID from './PlayerID';
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       input: '',
       submitted: false,
-      player: {}
-    }
+      player: {},
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
-    
 
-  handleChange(id){
+  handleChange(id) {
     this.setState({
       ...this.state,
-      input: id
-    })
+      input: id,
+    });
   }
 
-  handleClick(){
+  handleClick() {
     fetch(`http://localhost:5000/?id=${this.state.input}`)
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         this.setState({
           ...this.state,
           submitted: true,
-          player: data
-        })
-      })
+          player: data,
+        });
+      });
   }
 
-  render(){
-    if(this.state.submitted === false){
-      return(
-        <PlayerID change={this.handleChange} click={this.handleClick}/>
-      )
-    }else{
-      return(
-        <PlayerPage player={this.state.player} />
-      )
+  render() {
+    if (this.state.submitted === false) {
+      return <PlayerID change={this.handleChange} click={this.handleClick} />;
+    } else {
+      return <PlayerPage player={this.state.player} />;
     }
-    
   }
 }
 
-
-
-
 export default App;
-
